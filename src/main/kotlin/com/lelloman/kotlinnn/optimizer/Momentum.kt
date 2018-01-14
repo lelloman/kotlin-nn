ac74dd3b47e9a3f8acdb846ade7dd0a8877e5fc4
lelloman <cerasuolodomenico@gmail.com>
2018-01-14 11:53:20 +0100

    removed optional momentum params for Momentum optimizer

--- src/main/kotlin/com/lelloman/kotlinnn/optimizer/Momentum.kt
+++ src/main/kotlin/com/lelloman/kotlinnn/optimizer/Momentum.kt
@@ -2,7 +2,7 @@ package com.lelloman.kotlinnn.optimizer
 
 import java.util.*
 
-class Momentum(eta: Double = 0.01, private val momentum: Double? = null) : SGD(eta) {
+class Momentum(eta: Double = 0.01, private val momentum: Double) : SGD(eta) {
 
     private val prevWeightGradients by lazy {
         Array(network.size, { DoubleArray(weightGradients[it].size) })
@@ -11,14 +11,14 @@ class Momentum(eta: Double = 0.01, private val momentum: Double? = null) : SGD(e
     override fun updateWeights() = (1 until network.size).forEach {
 
         val gradients = weightGradients[it]
-        momentum?.let { m ->
-            val prevGradients = prevWeightGradients[it]
-            prevGradients.forEachIndexed { gradientIndex, prevGradient ->
-                val currentGradient = gradients[gradientIndex] + prevGradient * m
-                gradients[gradientIndex] = currentGradient
-                prevGradients[gradientIndex] = currentGradient
-            }
+
+        val prevGradients = prevWeightGradients[it]
+        prevGradients.forEachIndexed { gradientIndex, prevGradient ->
+            val currentGradient = gradients[gradientIndex] + prevGradient * momentum
+            gradients[gradientIndex] = currentGradient
+            prevGradients[gradientIndex] = currentGradient
         }
+
         network.layerAt(it).deltaWeights(gradients)
 
         Arrays.fill(weightGradients[it], 0.0)
